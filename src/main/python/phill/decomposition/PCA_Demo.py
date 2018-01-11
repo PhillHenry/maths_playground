38e204692bd06c98345e28443936944c7efa3e1a
PhillHenry <phillhenry@gmail.com>
2018-01-11 05:46:51 +0000

    Graphics.

--- src/main/python/phill/decomposition/PCA_Demo.py
+++ src/main/python/phill/decomposition/PCA_Demo.py
@@ -25,6 +25,7 @@ def plot_3d_matrix(m, ax, col):
         v = m[:, i]
         print "v", v
         ax.plot([0, v[0]], [0, v[1]], zs=[0, v[2]], color=col)
+        # ax.quiver(0, 0, 0, v[0], v[1], v[2]) # see https://gist.github.com/sytrus-in-github/a3b2ef4414fb144cb08505a060c99b18
 
 
 def scaled(vectors, values):
@@ -62,10 +63,26 @@ if __name__ == "__main__":
     # print "vt^-1", np.linalg.inv(vt)
 
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+
+    orig_evals, orig_evecs = np.linalg.eig(X)
+    ax2 = fig.add_subplot(211, projection='3d')
+    plot_3d_matrix(X, ax2, "blue")
+    plot_3d_matrix(scaled(orig_evecs, orig_evals), ax2, "red")
+    # ax2.text(3, 8, 'eigenvectors', style='italic', bbox={'facecolor':'red', 'alpha':0.5, 'pad':10} )
+    # ax2.text(3, 8, None, 'eigenvectors', 'italic')
+    ax2.set_title('Original')
+    ax2.text(0.1, 1, 0.1, 'Eigenvectors',
+            transform=ax2.transAxes,
+            color='red', fontsize=10)
+    ax2.text(0.1, 1, 1, 'Vectors',
+            transform=ax2.transAxes,
+            color='blue', fontsize=10)
+
+    ax = fig.add_subplot(212, projection='3d')
+    ax.set_title('Covariance')
+    # ax = Axes3D(fig)
     # plot_3d_matrix(via_svd, ax, "red")
 
-    orig_evals, orig_evacs = np.linalg.eig(X)
     # plot_3d_matrix(orig_evacs, ax, "grey")
     # plot_3d_matrix(evecs, ax, "green")
     # plot_3d_matrix(u, ax, "grey")
@@ -76,7 +93,19 @@ if __name__ == "__main__":
     #  so x.v ~= x.u
     plot_3d_matrix(res, ax, "blue")
     plot_3d_matrix(scaled(evecs, evals), ax, "green")
-    plot_3d_matrix(np.dot(x, u), ax, "magenta") # in the same plane as res
+    plot_3d_matrix(np.dot(x, u), ax, "magenta")  # in the same plane as res
+
+
+    ax.text(0.1, 1, 0.1, 'Eigenfaces',
+             # transform=ax.transAxes,
+             color='green', fontsize=10)
+    ax.text(0.1, 1, 0.5, 'SVD',
+             # transform=ax.transAxes,
+             color='magenta', fontsize=10)
+    # ax.text(0.1, 1, 13, 'sklearn',
+    #          transform=ax2.transAxes,
+    #          color='blue', fontsize=10)
+
     # plot_3d_matrix(np.dot(X, u), ax, "magenta") # same as X * vt.T
     # Conclusion: if we take u,s,VT via SVD of the covariance matrix, C, and the eigenvectors of C (call them v) then C.u = C.v
     # the point is that both SVD and eigendecomposition is on the *covariance* matrix not the original matrix.
