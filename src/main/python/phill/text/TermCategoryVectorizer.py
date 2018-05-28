03105cc5e8ec2c67131bd5dcf8270bb8f738e555
PhillHenry <phillhenry@gmail.com>
2018-05-28 12:36:10 +0100

    First attempt at a roll-your-own term-category matrix.

--- src/main/python/phill/text/TermCategoryVectorizer.py
+++ src/main/python/phill/text/TermCategoryVectorizer.py
@@ -1,7 +1,44 @@
 import collections
 
+import numpy as np
+
+
+def normalize(histo, size):
+    xs = [0.] * size
+    z = 0.0
+    for v in histo.values():
+        z = z + v ** 2
+    length = z ** 0.5
+    for k, v in histo.items():
+        xs[k] = v / length
+    return xs
+
+
+def to_category_vectors(doc_to_cat):
+    t_to_cs = term_to_cat_dict(doc_to_cat)
+    cats = set(doc_to_cat.values())
+    word_vectors = {}
+    size = len(cats)
+
+    for w, h in t_to_cs.items():
+        v = normalize(h, size)
+        word_vectors[w] = v
+
+    vs = []
+    for d in doc_to_cat.keys():
+        vector = np.zeros(size)
+        for w in d.split(" "):
+            v = np.add(vector, word_vectors[w])
+        vs.append(v)
+
+    return vs
+
 
 def term_to_cat_dict(doc_to_cat):
+    """
+    :param doc_to_cat: (text, cat id) tuples
+    :return: map of term to category histograms
+    """
     t_to_cs = collections.defaultdict(lambda: collections.defaultdict(lambda: 0))
     for d, c in doc_to_cat.items():
         print("d=", d, "c=",c)
--- src/test/python/phill/text/TestTermCategoryVectorizer.py
+++ src/test/python/phill/text/TestTermCategoryVectorizer.py
@@ -2,16 +2,32 @@ import unittest
 
 import src.main.python.phill.text.TermCategoryVectorizer as to_test
 import src.main.python.phill.tf.MySubjects as util
+import numpy as np
+
 
 class TestTermCategoryVectorizer(unittest.TestCase):
 
-    doc_to_cat      = {"In the beginning": 1, "God created the heavens and the Earth": 1, "In a hole in the ground": 2, "It was the best of times": 3}
+    doc_to_cat      = {"In the beginning": 0, "God created the heavens and the Earth": 0, "In a hole in the ground": 1, "It was the best of times": 2}
     all_docs        = doc_to_cat.keys()
     all_cats        = doc_to_cat.values()
     unique_cats     = set(all_cats)
-    #unique_words    = set([util.cleaned(ws) for d in all_docs for ws in d.split(" ")])
     unique_words    = set([ws for d in all_docs for ws in d.split(" ")])
-    print("unique words: ", unique_words)
+
+    def test_normalize(self):
+        n = 10
+        vector = {1: 5, 2: 4, 7: 6}
+        vec = to_test.normalize(vector, n)
+        self.assertEqual(len(vec), n)
+        self.is_normal(vec)
+
+    def test_docs_to_vectors(self):
+        vectors = to_test.to_category_vectors(self.doc_to_cat)
+        self.assertEqual(len(vectors), len(self.all_docs))
+        for vec in vectors:
+            self.assertEqual(len(vec), len(self.unique_cats))
+            x = np.asarray(vec)
+            print("x=", x)
+            self.is_normal(x)
 
     def test_terms_to_cats(self):
         print("docs", self.doc_to_cat)
@@ -20,3 +36,6 @@ class TestTermCategoryVectorizer(unittest.TestCase):
         print(keys)
         self.assertEqual(len(t2c), len(self.unique_words))
         self.assertEqual(set(keys), self.unique_words)
+
+    def is_normal(self, xs):
+        self.assertAlmostEqual(sum(map(lambda x: x**2, xs)), 1.0)
