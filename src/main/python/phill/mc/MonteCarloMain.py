6c829697228b1d85b8a9e8c1e0d24d174311a3f5
PhillHenry <phillhenry@gmail.com>
2018-07-10 08:22:03 +0100

    Comments added.

--- src/main/python/phill/mc/MonteCarloMain.py
+++ src/main/python/phill/mc/MonteCarloMain.py
@@ -2,6 +2,9 @@ import pymc3 as pm
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Initially, this gave me ""must use protocol 4 or greater to copy this object""
+# but it was cured with:
+# sudo pip3 install -U git+https://github.com/pymc-devs/pymc3.git
 if __name__ == '__main__':
     l1 = [0.9480880153813288, 0.9508302781971102, 0.9526527161276686,
           0.951900861163621, 0.9492505353319057, 0.9510564898663217,
@@ -16,12 +19,13 @@ if __name__ == '__main__':
     l1_std_obs = np.std(l1)
     l2_std_obs = np.std(l2)
 
+    # See chapter 2 of Bayesian Methods for Hackers
     with pm.Model() as model:
         l1_mean = pm.Uniform("l1_mean", 0.9, 0.99)
         l2_mean = pm.Uniform("l2_mean", 0.9, 0.99)
 
-        l1_std = pm.Uniform("l1_std", 0.0, 0.008)
-        l2_std = pm.Uniform("l2_std", 0.0, 0.008)
+        l1_std = pm.Uniform("l1_std", 0.0, 0.02)
+        l2_std = pm.Uniform("l2_std", 0.0, 0.02)
 
         l1_norm = pm.Normal("l1", l1_mean, l1_std, observed=l1)
         l2_norm = pm.Normal("l2", l2_mean, l2_std, observed=l2)
@@ -33,7 +37,8 @@ if __name__ == '__main__':
         burned_trace = trace[1000:]
 
         delta_samples = burned_trace["delta"]
-        plt.hist(delta_samples, histtype='stepfilled', bins=30, alpha=0.85,
-                 label="posterior of delta", color="#7A68A6", normed=True)
 
-    print("done")
+    plt.hist(delta_samples, histtype='stepfilled', bins=30, alpha=0.85,
+             label="posterior of delta", color="#7A68A6", normed=True)
+
+    plt.show()
