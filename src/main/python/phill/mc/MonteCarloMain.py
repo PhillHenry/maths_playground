9a1a50fe926993169eb5ce7ccf516bd13603a32c
PhillHenry <phillhenry@gmail.com>
2018-07-12 11:30:53 +0100

    Added some stats.

--- src/main/python/phill/mc/MonteCarloMain.py
+++ src/main/python/phill/mc/MonteCarloMain.py
@@ -2,22 +2,26 @@ import pymc3 as pm
 import numpy as np
 import matplotlib.pyplot as plt
 
+l1 = [0.9480880153813288, 0.9508302781971102, 0.9526527161276686,
+      0.951900861163621, 0.9492505353319057, 0.9510564898663217,
+      0.9427974947807933, 0.9433445661331087, 0.9472660471147611]
+l2 = [0.9351491569390402, 0.9421977556637731, 0.9387062566277836,
+      0.9475262368815592, 0.9456567352077213, 0.9437367303609342,
+      0.947907949790795, 0.9440574203082119, 0.9464398235664776]
+
+l1_mean_obs = np.mean(l1)
+l2_mean_obs = np.mean(l2)
+
+l1_std_obs = np.std(l1)
+l2_std_obs = np.std(l2)
+
+print("L1: mean {0:.3f}, std {1:.4f} ".format(l1_mean_obs, l1_std_obs))
+print("L2: mean {0:.3f}, std {1:.4f} ".format(l2_mean_obs, l2_std_obs))
+
 # Initially, this gave me ""must use protocol 4 or greater to copy this object""
 # but it was cured with:
 # sudo pip3 install -U git+https://github.com/pymc-devs/pymc3.git
 if __name__ == '__main__':
-    l1 = [0.9480880153813288, 0.9508302781971102, 0.9526527161276686,
-          0.951900861163621, 0.9492505353319057, 0.9510564898663217,
-          0.9427974947807933, 0.9433445661331087, 0.9472660471147611]
-    l2 = [0.9351491569390402, 0.9421977556637731, 0.9387062566277836,
-          0.9475262368815592, 0.9456567352077213, 0.9437367303609342,
-          0.947907949790795, 0.9440574203082119, 0.9464398235664776]
-
-    l1_mean_obs = np.mean(l1)
-    l2_mean_obs = np.mean(l2)
-
-    l1_std_obs = np.std(l1)
-    l2_std_obs = np.std(l2)
 
     # See chapter 2 of Bayesian Methods for Hackers
     with pm.Model() as model:
--- src/main/python/phill/mc/PlotDataMain.py
+++ src/main/python/phill/mc/PlotDataMain.py
@@ -1,14 +1,34 @@
-# import .MonteCarloMain as mc
 import os, sys
 sys.path.insert(0, os.path.abspath("."))  # https://stackoverflow.com/questions/9427037/relative-path-not-working-even-with-init-py
-from MonteCarloMain import l1
-from MonteCarloMain import l2
-import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
+import MonteCarloMain as data
+import numpy as np
+
+
+print(stats.ttest_ind(data.l1, data.l2, equal_var=False))
+
+# this means my calculation agrees with stats.ttest_ind
+vn1 = np.var(data.l1, 0, ddof=1) / len(data.l1)
+vn2 = np.var(data.l2, 0, ddof=1) / len(data.l2)
+
+# this doesn't quite give the correct value as the degrees of freedom is 0
+vn1 = data.l1_std_obs ** 2 / len(data.l1)
+vn2 = data.l2_std_obs ** 2 / len(data.l2)
+
+# this also is correct
+vn1 = np.var(data.l1, ddof=1) / len(data.l1)
+vn2 = np.var(data.l2, ddof=1) / len(data.l2)
+
+t = (data.l1_mean_obs - data.l2_mean_obs) / (vn1 + vn2) ** 0.5
+print("t = ", t)
 
 
 if __name__ == '__main__':
-    plt.plot(l1, np.zeros_like(l1) + 0.0, 'x')
-    plt.plot(l2, np.zeros_like(l2) + 0.1, 'x')
-    plt.axis('off')
+    # https://stackoverflow.com/questions/23546552/1d-plot-matplotlib
+    plt.hlines(1, min(min(data.l1), min(data.l2)), max(max(data.l1), max(data.l2)))  # Draw a horizontal line
+    plt.eventplot(data.l1, orientation='horizontal', colors='b')
+    plt.eventplot(data.l2, orientation='horizontal', colors='r')
+    ax = plt.gca()  # https://stackoverflow.com/questions/3886255/how-do-i-remove-the-y-axis-from-a-pylab-generated-picture
+    ax.yaxis.set_visible(False)
     plt.show()
