11cd945d9d0dd33b0fd4858f5852ebb5578cb16b
PhillHenry <phillhenry@gmail.com>
2018-08-21 10:58:42 +0100

    Refactor.

--- src/main/python/phill/stats/MyTTestQuestion.py
+++ src/main/python/phill/stats/MyTTestQuestion.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
         y = np.random.normal(mean, sigma_x, size=(Nsamp))
         (t_value, p_value) = stats.ttest_ind(x, y, equal_var=True)
         print("t-test compared to same distribution but different data, t = {0:.3f}, p = {1:.3f}".format(t_value, p_value))
-        ps.append((p_value))
+        ps.append(p_value)
     p_mean = sum(ps) / len(ps)
     print("p-values: Average {0:.3f} lowest {1:.3f}".format(p_mean, min(ps)))
