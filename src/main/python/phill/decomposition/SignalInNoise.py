c9f8643239fc23e6e069a6a7207c6ad86281b8f1
PhillHenry <phillhenry@gmail.com>
2019-04-19 09:47:36 +0100

    Exact replica

--- src/main/python/phill/decomposition/SignalInNoise.py
+++ src/main/python/phill/decomposition/SignalInNoise.py
@@ -13,28 +13,33 @@ rys = np.random.randint(h, size=n)
 xs = np.append(np.arange(0, h), rxs)
 ys = np.append(np.arange(0, h), rys)
 
-#plt.scatter(xs, ys, marker="+")
-#plt.show()
+# plt.scatter(xs, ys, marker="+")
+# plt.show()
 
-X = np.zeros(shape=[h, w])
-for (x, y) in zip(xs, ys):
-    X.itemset((x,y), 1)
+# X = np.zeros(shape=[h, w])
+# for (x, y) in zip(xs, ys):
+#     X.itemset((x, y), 1)
+
+X = np.asmatrix(np.array([xs, ys]))
 
 # U, Sigma, VT = svds(X, k=2, tol=0)
 U, Sigma, VT = np.linalg.svd(X, full_matrices=True)
 
-S = np.diag(Sigma)
+# S = np.diag(Sigma)
 
-# S = np.zeros(shape=[w, h])
-# for i in range(len(Sigma)):
-#     S.itemset((i, i), Sigma[i])
+S = np.zeros(shape=[U.shape[1], VT.shape[0]])
+for i in range(len(Sigma)):
+    S.itemset((i, i), Sigma[i])
 
 uv = np.dot(U, S)
 all = np.dot(uv, VT)
-print('U.shape = {}, Sigma.shape = {}, S.shape ={}, uv.shape = {}, VT.shape = {}, all.shape = {}'.format(U.shape, Sigma.shape, S.shape, uv.shape, VT.shape, all.shape))
+print('U.shape = {}, Sigma.shape = {}, S.shape ={}, uv.shape = {}, VT.shape = {}, all = {}'.format(U.shape, Sigma.shape, S.shape, uv.shape, VT.shape, all.shape))
 
-x,y = np.argwhere(X != 0).T
-print(x)
+# x,y = np.argwhere(X != 0).T
+ptx = np.asarray(all[0, :])[0]
+pty = np.asarray(all[1, :])[0]
+print('ptx.shape = {}, pty.shape = {}'.format(len(ptx), len(pty)))
 
-plt.scatter(x,y)
+plt.scatter(ptx, pty)
 plt.show()
+print("finished")
