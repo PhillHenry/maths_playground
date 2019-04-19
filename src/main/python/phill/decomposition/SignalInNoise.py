84993ee82588a052232bfbc6e0de7cf781473c2e
PhillHenry <phillhenry@gmail.com>
2019-04-19 09:53:39 +0100

    First eigenvalue indeed gives the anomaly.

--- src/main/python/phill/decomposition/SignalInNoise.py
+++ src/main/python/phill/decomposition/SignalInNoise.py
@@ -13,13 +13,6 @@ rys = np.random.randint(h, size=n)
 xs = np.append(np.arange(0, h), rxs)
 ys = np.append(np.arange(0, h), rys)
 
-# plt.scatter(xs, ys, marker="+")
-# plt.show()
-
-# X = np.zeros(shape=[h, w])
-# for (x, y) in zip(xs, ys):
-#     X.itemset((x, y), 1)
-
 X = np.asmatrix(np.array([xs, ys]))
 
 # U, Sigma, VT = svds(X, k=2, tol=0)
@@ -28,8 +21,8 @@ U, Sigma, VT = np.linalg.svd(X, full_matrices=True)
 # S = np.diag(Sigma)
 
 S = np.zeros(shape=[U.shape[1], VT.shape[0]])
-for i in range(len(Sigma)):
-    S.itemset((i, i), Sigma[i])
+
+S.itemset((0, 0), Sigma[0])
 
 uv = np.dot(U, S)
 all = np.dot(uv, VT)
@@ -40,6 +33,11 @@ ptx = np.asarray(all[0, :])[0]
 pty = np.asarray(all[1, :])[0]
 print('ptx.shape = {}, pty.shape = {}'.format(len(ptx), len(pty)))
 
+
+fig = plt.figure(0)
+fig.add_subplot(121)
+plt.scatter(xs, ys, marker="+")
+fig.add_subplot(122)
 plt.scatter(ptx, pty)
 plt.show()
 print("finished")
