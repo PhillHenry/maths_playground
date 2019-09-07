66c9544675fd1e7f336384d5afbc4ba46165951f
PhillHenry <phillhenry@gmail.com>
2019-09-07 09:26:16 +0100

    Added legend.

--- src/main/python/phill/myscipy/MyBayes.py
+++ src/main/python/phill/myscipy/MyBayes.py
@@ -3,15 +3,15 @@ import numpy as np
 import scipy.stats as stats
 
 if __name__ == "__main__":
-    print("hello world")
     dist = stats.beta
     x = np.linspace(0, 1, 100)
 
     b_2_5 = dist.pdf(x, 2, 5)
     b_65_42 = dist.pdf(x, 65, 42)
 
-    plt.plot(x, b_2_5, label="beta(2, 5)")
-    plt.plot(x, b_65_42, label="beta(65, 42)")
-    plt.title("Fig 9.9 from PPP")
+    plt.plot(x, b_2_5, label="Prior [beta(2, 5)]", c='r')
+    plt.plot(x, b_65_42, label="Posterior [beta(65, 42)]", c='b')
+    plt.title("Bias and Posterior")
+    plt.legend(loc='upper left')
 
     plt.show()
