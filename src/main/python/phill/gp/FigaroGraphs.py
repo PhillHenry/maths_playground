21ab8be1510fca557d89841b7fa945ad3a751882
Phillip Henry <PhillHenry@gmail.com>
2020-09-13 15:22:50 +0100

    Added title.

--- src/main/python/phill/gp/FigaroGraphs.py
+++ src/main/python/phill/gp/FigaroGraphs.py
@@ -6,9 +6,9 @@ from mpl_toolkits.mplot3d import Axes3D
 
 # converted from https://github.com/p2t2/figaro/blob/7bdc7c26b011633b4e0a66decc068ffa6f8177f2/FigaroExamples/src/main/scala/com/cra/figaro/example/GaussianProcessTraining.scala
 
-def plot(xs, ys, z, where):
+def plot(xs, ys, z, where, title):
     x, y = np.meshgrid(xs, ys)
-    ax = pl.subplot(where, projection='3d')
+    ax = pl.subplot(where, projection='3d', title=title)
     ax.plot_surface(x, y, z)
 
 
@@ -23,7 +23,10 @@ if __name__ == "__main__":
 
     C = np.dot(X, X.T)
 
+    invC = np.linalg.inv(C + (np.eye(len(xs)) * 0.001)) # https://javaagile.blogspot.com/2017/12/ridge-regression.html
+
     pl.figure()
-    plot(xs, ys, C, 111)
+    plot(xs, ys, C, 121, "Covariant")
+    plot(xs, ys, invC, 122, "Inverse Covariant with bias")
     pl.show()
 
