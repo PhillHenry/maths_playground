05656742fa64f86321f47b9ad1963cfbf5d2fb06
Phillip Henry <PhillHenry@gmail.com>
2020-09-15 11:24:03 +0100

    Heatmaps; element-wise multiplication.

--- src/main/python/phill/gp/FigaroGraphs.py
+++ src/main/python/phill/gp/FigaroGraphs.py
@@ -3,6 +3,7 @@ from random import gauss
 import matplotlib.pyplot as pl
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+from os.path import expanduser
 
 # converted from https://github.com/p2t2/figaro/blob/7bdc7c26b011633b4e0a66decc068ffa6f8177f2/FigaroExamples/src/main/scala/com/cra/figaro/example/GaussianProcessTraining.scala
 
@@ -25,26 +26,34 @@ def covariance_ls(i, j, xs, gamma):
 
 def radial_basis_function_kernel(xs, gamma):
     size = len(xs)
-    M = [covariance_ls(i, j, xs, gamma) for i in range(0, size) for j in range(0, size)]
-    return np.asmatrix(M).reshape([size, size])
+    m = [covariance_ls(i, j, xs, gamma) for i in range(0, size) for j in range(0, size)]
+    return np.asmatrix(m).reshape([size, size])
+
+
+def heat_map(m, file):
+    dir = expanduser("~") + "/Pictures/"
+    heatmap = pl.imshow(m, cmap='hot', interpolation='nearest')
+    pl.colorbar(heatmap)
+    pl.savefig(dir + "/" + file)
+    pl.clf()
 
 
 if __name__ == "__main__":
     xs = np.arange(1, 11).reshape([10, 1])
-    ys = list(map(lambda x: (x ** 2) + gauss(0, 1), xs))
+    ys = np.asmatrix(list(map(lambda x: (x ** 2) + gauss(0, 1), xs)))
+    print("ys shape = ", np.shape(ys))
 
-    print("xs = ", xs)
-    print("ys = ", ys)
-
-    x = radial_basis_function_kernel(xs, 0.05)
-    print("x = ", x)
-    C = covariant_matrix_of(x)
+    C = radial_basis_function_kernel(xs, 0.05)
+    print("C shape = ", np.shape(C))
 
     invC = np.linalg.inv(C + (np.eye(len(xs)) * 0.001)) # https://javaagile.blogspot.com/2017/12/ridge-regression.html
+    print("invC shape = ", np.shape(invC))
+
+    alpha = np.multiply(invC, np.transpose(ys))
+    print("alpha shape = ", np.shape(alpha))
 
     pl.figure()
-    plot(xs, ys, x, 311, "Kernel")
-    plot(xs, ys, C, 312, "Covariant")
-    plot(xs, ys, invC, 313, "Inverse Covariant with bias")
-    pl.show()
+    heat_map(C, "/m_covariance_fn.png")
+    heat_map(invC, "/m_inv_covariance_fn.png")
+    heat_map(alpha, "/m_alpha.png")
 
